60c85f4debef4d55e2ed380745ca6b931efc7483
Vincent <v.nassaux@nootos.com>
2018-05-30 23:54:13 +0200

    MethodeA

---
+++ MethodeCSharpBases2/MethodeCSharpBases2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodeCSharpBases2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AffichageBienvenue();
+            AffichageBienvenue();
+        }
+
+        static void AffichageBienvenue()   // j'ai supprimé l'origine : (string[] args)
+        {
+            Console.WriteLine("Bonjour Nicolas");
+            Console.WriteLine("------------------" + Environment.NewLine);
+            Console.WriteLine("\tBienvenue dans le monde merveilleux du C#");
+        }
+    }
+}
