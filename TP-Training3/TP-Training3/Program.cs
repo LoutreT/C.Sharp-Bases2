65d17ab65e463c643ccde654143d05e4ba3f4366
Vincent <v.nassaux@nootos.com>
2018-06-26 23:44:19 +0200

    TP-Training

---
+++ TP-Training3/TP-Training3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Training3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday && DateTime.Now.Hour >= 17 || DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("Bon Week-End !");
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Bonne fin de week-end !");
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday || DateTime.Now.DayOfWeek == DayOfWeek.Tuesday || DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("Bonne semaine !");
+            }
+            else
+            {
+                Console.WriteLine("Bonne fin de semaine !");
+            }
+        }
+    }
+}
