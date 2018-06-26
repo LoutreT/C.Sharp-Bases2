f414fe61175789ddc6c895e767d1cccf813209f1
Vincent <v.nassaux@nootos.com>
2018-06-26 07:30:48 +0200

    Training2

---
+++ TP-Training2/TP-Training2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Training2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("Bonne journée");
+            }
+            else
+            {
+                Console.WriteLine("Bonne semaine");
+            }
+        }
+    }
+}
