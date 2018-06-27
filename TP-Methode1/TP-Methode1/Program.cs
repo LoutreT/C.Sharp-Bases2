d259f6592c971b54aeb9d6e84487924ae2c5c639
Vincent <v.nassaux@nootos.com>
2018-06-27 06:25:19 +0200

    BaseLancementTest

---
+++ TP-Methode1/TP-Methode1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Methode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int CalculTotal(int Min, int Max)
+        { 
+            int Total = 0;
+            List<int> Pouraditionner = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int element in Pouraditionner)
+            {
+                Total += element;
+            }
+            return Total;
+        }
+
+    }
+}
