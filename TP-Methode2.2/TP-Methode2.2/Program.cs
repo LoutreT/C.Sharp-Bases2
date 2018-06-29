ed893217453440b1417ad43fc64e9c95a3a2aae6
Vincent <v.nassaux@nootos.com>
2018-06-29 08:45:27 +0200

    TP-Methode2-2

---
+++ TP-Methode2.2/TP-Methode2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Methode2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> liste = new List<double> { 1.0, 5.5, 9.9, 2.8, 9.6 };
+            {
+                double somme = 0;
+
+                foreach (double valeur in liste)
+                {
+                    somme += valeur;
+                }
+                Console.WriteLine(somme / liste.Count);    // ici je le divise 
+                          // en 5 éléments consituant la liste et donc = 5,76
+            }
+        
+        }
+    }
+}
