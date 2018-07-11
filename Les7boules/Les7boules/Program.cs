bca10c6e1bedeeb44aaa7e601c0a3146ba7d8223
Vincent <v.nassaux@nootos.com>
2018-07-11 23:59:12 +0200

    Evolution2

---
+++ Les7boules/Les7boules/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les7boules
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau = new int[50];
+
+            int i = 1;
+            for (i = 0; i <= 50; i++)
+            {
+                Console.Write(tableau[i]);
+                i++;
+            }
+        }
+    }
+}
